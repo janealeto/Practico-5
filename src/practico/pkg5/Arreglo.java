c23b14f89418edb89422800e6332128c1a5d553a
janealeto <alejaneto@gmail.com>
2021-04-16 12:47:42 -0300

    primer commit

---
+++ src/practico/pkg5/Arreglo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practico.pkg5;
+
+/**
+ *
+ * @author Jannete
+ */
+public class Arreglo {
+ 
+//reciba por parámetro un arreglo unidimensional de enteros y muestre por pantalla la suma y promedio de los mismos.
+//    static public void sumarLista(int[]lista){
+//        int suma=0;
+//        double promedio;
+//    }
+//    
+////Método buscarMayor que reciba por parámetro un arreglo bidimensional e irregular de enteros y retorne el entero más grande que se encuentra en el arreglo
+//    static public void buscarMayor(int[]lista){
+//        
+//    }
+    
+//Método cuentaVocales, que reciba por parámetro un String y retorne la cantidad de vocales que tiene la cadena
+    static public void cuentaVocales(String palabra){
+        String []letraxletra=palabra
+                
+}
+}
