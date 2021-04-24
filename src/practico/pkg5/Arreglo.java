c4e36c4109d4f1887cbff1109889e42d9cf5c017
janealeto <alejaneto@gmail.com>
2021-04-24 13:09:31 -0300

    Practico N°5 finalizado

--- src/practico/pkg5/Arreglo.java
+++ src/practico/pkg5/Arreglo.java
@@ -12,19 +12,69 @@ package practico.pkg5;
 public class Arreglo {
  
 //reciba por parámetro un arreglo unidimensional de enteros y muestre por pantalla la suma y promedio de los mismos.
-//    static public void sumarLista(int[]lista){
-//        int suma=0;
-//        double promedio;
-//    }
-//    
+    static public int sumarLista(int[] numeros){
+        int suma = 0;
+        for(int i=0; i<numeros.length; i++ ){
+            suma += numeros[i];
+            System.out.println("Sumando numero: " +numeros[i]);
+        }
+       return suma;
+    }
+    
 ////Método buscarMayor que reciba por parámetro un arreglo bidimensional e irregular de enteros y retorne el entero más grande que se encuentra en el arreglo
-//    static public void buscarMayor(int[]lista){
-//        
-//    }
+    static public int buscarMayor(int[][] numeros){
+        int mayor=0;
+        
+        for(int i=0; i<numeros.length; i++){//lenght propion del array
+            for(int j=0; j<numeros[i].length; j++)//el segundo array recorre el primero "numeros--->[i]"
+            if(numeros[i][j]>mayor){//para acceder necesito las dos coordenadas "[..][..]"
+                mayor=numeros[i][j];
+            }
+        }
+        System.out.println("El entero mas grande es: "+mayor); 
+        return mayor;
+       
+    }
     
 //Método cuentaVocales, que reciba por parámetro un String y retorne la cantidad de vocales que tiene la cadena
-    static public void cuentaVocales(String palabra){
-        String []letraxletra=palabra;
-                
-}
-}
+    static public int cuentaVocales(String palabras){
+        int cantidadVocales=0;
+        palabras=palabras.toLowerCase();
+        for(int i=0; i<palabras.length(); i++){//lenght es un metodo de la clase String....?
+           switch (palabras.charAt(i)){//Obtiene el caracter en ese indice
+               case 'a':
+                   cantidadVocales+=1;
+                   break;
+                case 'e':
+                   cantidadVocales+=1;
+                   break;
+                case 'i':
+                   cantidadVocales+=1;
+                   break;
+                case 'o':
+                   cantidadVocales+=1;
+                   break;
+                case 'u':
+                   cantidadVocales+=1;
+                   break;
+            
+        }
+               
+    }
+        return cantidadVocales; 
+    }
+ //que reciba por parámetro un String y un caracter, luego retorne la cantidad de veces que se repite en la cadena el carácter recibido.   
+    static public int  cuentaCaracter(String a, char b){
+       int cantidadCaracter = 0;
+         for(int i=0; i<a.length(); i++){
+             if(a.charAt(i)==b){
+                 
+                cantidadCaracter+=1;
+                        
+             }
+         }
+        
+        
+     return cantidadCaracter;   
+    }  
+   }
--- src/practico/pkg5/Practico5.java
+++ src/practico/pkg5/Practico5.java
@@ -15,7 +15,48 @@ public class Practico5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //nombre del arreglo: num
+        int[] num = new int[8];
+    // en el elemento le asigno el valor
+        num[0] = 8;
+        num[1] = 1;
+        num[2] = 5;
+        num[3] = 9;
+        num[4] = 2;
+        
+        int suma = Arreglo.sumarLista(num);
+        System.out.println("El promedio es: "+suma/num.length);
+        System.out.println("La suma es: "+suma);
+        
+        int[][] num1 = new int[5][];//lo que pase al primero vector va a ser la cantidad de new que voy a tener que hacer
+    //Inicializando la matriz, el tamaño de filas y columnas, todavia sin valor
+        num1[0] = new int[2];
+        num1[1] = new int[4];
+        num1[2] = new int[1];
+        num1[3] = new int[2];
+        num1[4] = new int[1];
+        
+     //aca le doy las "coordenadas" a la matriz para ingresar el valor
+        num1[0][0] = 5;
+        num1[0][1] = 3;
+        num1[1][2] = 2;
+        num1[1][3] = 1;
+        num1[1][1] = 4;
+        
+        Arreglo.buscarMayor(num1);
+      
+        int vocales = Arreglo.cuentaVocales("Esternocleidomastoideo");
+        
+        System.out.println("La cantidad de vocales es: " + vocales);
+          
+        int caracteres = Arreglo.cuentaCaracter("Janneteeeee", 'e');
+
+        System.out.println("La cantidad de veces que se repite el caracter es: " + caracteres);
+
+           
+        
+        
+        
     }
     
 }
